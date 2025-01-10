892eef1e3b44bf965ef4485b9ebd4aff62979e7e
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-10 16:08:51 +0100

    add serial flush to serial.hpp

--- include/serial.hpp
+++ include/serial.hpp
@@ -5,6 +5,7 @@
 
 #define serial_print(x) { if (serial_is_enabled()) { Serial.print(x); } }
 #define serial_println(x) { if (serial_is_enabled()) { Serial.println(x); } }
+#define serial_flush() { if (serial_is_enabled()) { Serial.flush(); } }
 
 void serial_begin(unsigned long baudrate = SERIAL_BAUD);
 void serial_end();
