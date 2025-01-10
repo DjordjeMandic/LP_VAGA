0a3f0a14d4a5373657154e0ebfab77e777113515
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-10 13:28:05 +0100

    add power down delay

---
+++ include/power/delay.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+void delay_80ms_power_down_adc_off_bod_on();
+void delay_95ms_power_down_adc_off_bod_on();
+void delay_125ms_power_down_adc_off_bod_on();
+void delay_185ms_power_down_adc_off_bod_on();
+void delay_315ms_power_down_adc_off_bod_on();
+void delay_565ms_power_down_adc_off_bod_on();
+void delay_1065ms_power_down_adc_off_bod_on();
+void delay_2065ms_power_down_adc_off_bod_on();
+void delay_4065ms_power_down_adc_off_bod_on();
+void delay_8065ms_power_down_adc_off_bod_on();
