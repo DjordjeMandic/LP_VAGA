8a722315bdf7c56b90858b45060039ae100475ac
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-10 16:34:33 +0100

    comment adc

--- include/power/adc.hpp
+++ include/power/adc.hpp
@@ -3,18 +3,43 @@
 #include <Arduino.h>
 #include <config.hpp>
 
+/**
+ * @brief Initializes the ADC by enabling power and setting the prescaler.
+ *        Uses AVcc as the reference voltage and configures the initial input channel (GND).
+ */
 void adc_begin();
 
+/**
+ * @brief Disables the ADC by clearing control registers and disabling power.
+ */
 void adc_end();
 
-uint16_t adc_sample();
-
+/**
+ * @brief Initializes the ADC for measuring the AVcc reference voltage.
+ *        The function sets the correct ADC MUX for AVcc measurement, performs a dummy sample,
+ *        and waits for the reference to stabilize.
+ * @param[in] stabilization_delay_ms Delay in milliseconds to allow Vref stabilization.
+ *        Defaults to @p `ADC_AVCC_REF_STABILIZATION_DELAY_MS` if not provided.
+ */
 void adc_avcc_init(uint8_t stabilization_delay_ms = ADC_AVCC_REF_STABILIZATION_DELAY_MS);
 
+/**
+ * @brief Performs a single ADC conversion with AVcc as the reference.
+ *        The function sets the correct MUX for AVcc measurement, performs a dummy sample,
+ *        and then returns the result of a new sample.
+ * @return 16-bit result of the ADC conversion.
+ * 
+ * @note This function assumes that the ADC is already initialized with `adc_avcc_init()`
+ */
 uint16_t adc_avcc_sample();
 
+/**
+ * @brief Computes the average of multiple ADC samples with AVcc as the reference.
+ *        A dummy sample is performed first to ensure stability, and then the specified number 
+ *        of samples is averaged.
+ * @param[in] samples Number of ADC samples to average. Defaults to @p `ADC_AVCC_SAMPLES_DEFAULT` if not provided.
+ * @return 16-bit average result of the ADC samples.
+ * 
+ * @note This function assumes that the ADC is already initialized with `adc_avcc_init()`
+ */
 uint16_t adc_avcc_sample_average(uint8_t samples = ADC_AVCC_SAMPLES_DEFAULT);
-
-uint16_t adc_avcc_noise_reduced_sample();
-
-uint16_t adc_avcc_noise_reduced_sample_average(uint8_t samples = ADC_AVCC_SAMPLES_DEFAULT);
--- src/power/adc.cpp
+++ src/power/adc.cpp
@@ -2,42 +2,67 @@
 #include <power/adc.hpp>
 #include <power/sleep.hpp>
 
+/* ADC conversion complete interrupt */
 EMPTY_INTERRUPT (ADC_vect);
 
+/* AVcc as reference and Vbg as input */
 #define adc_avcc_mux_set() (ADMUX = bit(REFS0) | bit(MUX3) | bit(MUX2) | bit(MUX1))
 
 void adc_begin()
 {
     power_adc_enable();
-    ADCSRA = _BV(ADPS2) | _BV(ADPS1) | _BV(ADEN); // 8 MHz / 64 = 125 KHz
-    ADMUX = bit(REFS0) | bit(MUX3) | bit(MUX2) | bit(MUX1) | bit(MUX0); // External reference connected to AREF pin and GND channel
+
+    /* 8 MHz / 64 = 125 KHz */
+    ADCSRA = _BV(ADPS2) | _BV(ADPS1) | _BV(ADEN);
+
+    /* External reference connected to AREF pin and GND channel */
+    ADMUX = bit(REFS0) | bit(MUX3) | bit(MUX2) | bit(MUX1) | bit(MUX0); 
 }
 
 void adc_end()
 {
     ADCSRA = 0;
     power_adc_disable();
-}
+} 
 
+/**
+ * @brief Performs a single ADC conversion on the current channel.
+ *        The function starts the conversion, puts the microcontroller into ADC noise reduction sleep mode, 
+ *        and returns the result.
+ * @return 16-bit result of the ADC conversion.
+ * 
+ * @note This function assumes that the ADC is already initialized with `adc_begin()`
+ */
 uint16_t adc_sample()
 {
-    bitSet(ADCSRA, ADSC); // Start the ADC conversion
-    loop_until_bit_is_clear(ADCSRA, ADSC); // Wait for the ADC conversion to complete
+    /* Start the ADC conversion, ADSC will be cleared once done */
+    bitSet(ADCSRA, ADSC);
+
+    /* Sleep until the conversion finishes (ADSC is cleared) */
+    sleep_while(SLEEP_MODE_ADC, bit_is_set(ADCSRA, ADSC));
+
     return ADC;
 }
 
 void adc_avcc_init(uint8_t stabilization_delay_ms)
 {
     adc_begin();
+    
+    /* Set adc mux and do one dummy sample */
     adc_avcc_mux_set();
     adc_sample();
+
+    /* Wait for Vref to stabilize */
     sleep_timeout_millis(SLEEP_MODE_ADC, stabilization_delay_ms);
 }
 
 uint16_t adc_avcc_sample()
 {
+    /* Set adc mux and do one dummy sample */
     adc_avcc_mux_set();
     adc_sample();
+
+    /* Return result of new sample */
     return adc_sample();
 }
 
@@ -45,11 +70,12 @@ uint16_t adc_avcc_sample_average(uint8_t samples)
 {
     if (samples == 0)
     {
-        samples++;
+        return 0;
     }
+
     uint32_t totalAdcReading = 0;
 
-    // Set adc mux and do one dummy sample
+    /*  Set adc mux and do one dummy sample */
     adc_avcc_mux_set();
     adc_sample();
 
@@ -58,46 +84,6 @@ uint16_t adc_avcc_sample_average(uint8_t samples)
         totalAdcReading += adc_sample();
     }
 
+    /* Return average */
     return (totalAdcReading + (samples / 2)) / samples;
-}
-
-uint16_t adc_avcc_noise_reduced_sample()
-{
-    // set mux and do one dummy sample
-    adc_avcc_mux_set();
-    adc_sample();
-
-    bitSet(ADCSRA, ADIF); // turn off any pending interrupt 
-    bitSet(ADCSRA, ADSC); // Start the ADC conversion
-
-    sleep_while(SLEEP_MODE_ADC, bit_is_set(ADCSRA, ADSC));
-
-    return ADC; // Return ADC result
-}
-
-uint16_t adc_avcc_noise_reduced_sample_average(uint8_t samples)
-{
-    if (samples == 0)
-    {
-        samples++;
-    }
-
-    uint32_t totalAdcReading = 0;
-
-    // Set adc mux and do one dummy sample
-    adc_avcc_mux_set();
-    adc_sample();
-
-    bitSet(ADCSRA, ADIF); // turn off any pending interrupt 
-
-    for (uint8_t i = 0; i < samples; i++)
-    {
-        bitSet(ADCSRA, ADSC); // Start the ADC conversion
-
-        sleep_while(SLEEP_MODE_ADC, bit_is_set(ADCSRA, ADSC));
-
-        totalAdcReading += ADC;
-    }
-
-    return (totalAdcReading + (samples / 2)) / samples; // Return ADC result
 }
