17bdbdf3b8dfab9e9c2836c868c24432012d68fd
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-11 21:35:09 +0100

    add libraries

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 #include <module/DHTModule.hpp>
 #include <module/RTCModule.hpp>
 #include <power/ADCHelper.hpp>
+#include <power/sleep.hpp>
+#include <DataEEPROM.hpp>
 #include <Serial.hpp>
 #include <RTClib.h>
 #include <DHT.h>
@@ -30,9 +32,6 @@
 
 // todo use WDT sleep delay for unimportant delays
 
-bool user_scale_tare();
-bool user_scale_calibration();
-
 void power_all_off();
 
 void show_result_final_block(bool success = false);
@@ -44,31 +43,49 @@ void setup()
     pinMode(BUTTON_CALIBRATE_PIN, BUTTON_PIN_MODE(BUTTON_CALIBRATE_PIN_ACTIVE_STATE));
     pinMode(BUTTON_TARE_PIN, BUTTON_PIN_MODE(BUTTON_TARE_PIN_ACTIVE_STATE));
 
+    /* set baud rate in Config.hpp */
+    serial_begin();
+    serial_println(F("Starting"));
+
+    builtin_led_on();
+
     bool calibrate_button_pressed = BUTTON_PRESSED(BUTTON_CALIBRATE_PIN, BUTTON_CALIBRATE_PIN_ACTIVE_STATE);
     bool tare_button_pressed      = BUTTON_PRESSED(BUTTON_TARE_PIN, BUTTON_TARE_PIN_ACTIVE_STATE);
 
     uint8_t button_pressed_count = calibrate_button_pressed + tare_button_pressed;
 
-    /* set baud rate in Config.hpp */
-    serial_begin();
-
     if (button_pressed_count > 1)
     {
-        /* both buttons pressed, block */
+        /* multiple buttons pressed, block */
         serial_println(F("Multiple buttons pressed"));
         show_result_final_block(false);
     }
 
+    /* calibrate internal adc reference */
+
+    //todo
+
+    /* check avcc voltage, power down if too low for modules to work */
+
+    //todo
+
+    /* calibrate or tare scale */
     if (calibrate_button_pressed || tare_button_pressed)
     {
-        serial_println(F("HX711 initializing"));
-        
+        if (serial_is_enabled())
+        {
+            Serial.println(F("Calibrate or tare operation requested"));
+            Serial.println(F("HX711 initializing"));
+        }
+
         /* power on scale and init library */
         ScaleModule::begin();
         bool scale_status = true;
 
         /* wait for scale to be ready */
         scale_status &= ScaleModule::waitReadyTimeout();
+
+        builtin_led_off();
         
         /* perform scale stabilization only if scale is ready */
         if (scale_status)
@@ -79,80 +96,116 @@ void setup()
             scale_status &= ScaleModule::stabilize();
         }
 
+        /* perform tare only if scale is stabilized */
+        if (scale_status)
+        {
+            serial_println(F("HX711 taring"));
+
+            /* tare operation is required anyways for calibration */
+            scale_status &= ScaleModule::tare();
+        }
+
         if (!scale_status)
         {
             /* failed to setup scale, block */
             show_result_final_block(false);
         }
 
-        if (BUTTON_PRESSED(BUTTON_TARE_PIN, BUTTON_TARE_PIN_ACTIVE_STATE))
-        {
-            /* tare scale */
+        /* save new tare offset */
+        long new_tare_offset = ScaleModule::getOffset();
 
-            /* block end of tare */
-            while (1);
+        if (serial_is_enabled())
+        {
+            Serial.print(F("Saving new tare offset: "));
+            Serial.println(new_tare_offset);
         }
-        else if (BUTTON_PRESSED(BUTTON_CALIBRATE_PIN, BUTTON_CALIBRATE_PIN_ACTIVE_STATE))
+
+        /* store to EEPROM */
+        DataEEPROM::setScaleTareOffset(new_tare_offset);
+
+        if (tare_button_pressed)
+        {
+            serial_println(F("Only tare operation requested"));
+
+            /* operation successful */
+            show_result_final_block(true);
+        }
+
+        if (calibrate_button_pressed)
         {
             /* calibrate scale */
+            serial_println(F("Calibrating scale"));
+
+            float known_mass_kg = SCALE_CALIBRATION_KNOWN_MASS_KG;
+
+            /* sleep for 2 seconds */
+            sleep_power_down_2065ms_adc_off_bod_on();
+
+            /* wait for user to release calibrate button if not released yet */
+            sleep_while(SLEEP_MODE_IDLE, BUTTON_PRESSED(BUTTON_CALIBRATE_PIN, BUTTON_CALIBRATE_PIN_ACTIVE_STATE));
+
+            /* wait for user to place known weight and press calibrate button */
+            do
+            {
+                if (serial_is_enabled())
+                {
+                    Serial.print(F("Press calibrate button after "));
+                    Serial.print(known_mass_kg, 0);
+                    Serial.println(F("kg weight is placed"));
+                }
+
+                builtin_led_on();
+                sleep_power_down_185ms_adc_off_bod_on();
+                builtin_led_off();
+                sleep_power_down_1065ms_adc_off_bod_on();
+            } while (!BUTTON_PRESSED(BUTTON_CALIBRATE_PIN, BUTTON_CALIBRATE_PIN_ACTIVE_STATE));
+            
+            // Call setScale() with no parameter.
+            // Call tare() with no parameter.
+            // Place a known weight on the scale and call getUnits(10).
+            // Divide the result in step 3 to your known weight. You should get about the parameter you need to pass to setScale().
+            // Adjust the parameter in step 4 until you get an accurate reading.
+
+            /* set scale factor to 1 */
+            ScaleModule::setScale(1.0f);
+
+            float new_scale_factor = ScaleModule::getUnits(10) / known_mass_kg;
+
+            DataEEPROM::setScaleCalibrationValue(new_scale_factor);
+
+            serial_print(F("New scale factor: "));
+            serial_println(new_scale_factor);
 
             /* block end of calibration */
-            while (1);
+            show_result_final_block(true);
         }
-    }
 
-    scale_begin();
-    serial_begin();
-    serial_println(F("Start..."));
-    while(!hx711_power_delay_check())
-    {
-        serial_println(F("Waiting for HX711 to power on..."));
-    }
-    if (!scale_wait_ready_timeout(1000))
-    {
-        while (1)
-        {
-            digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-            delay(500);
-        }
+        /* operation unknown */
+        serial_println(F("Unknown scale operation"));
+        show_result_final_block(false);
     }
 }
 
 void loop()
 {
-    for(uint8_t i = 0; i < 100; i++) {
-        if (scale_ready()) {
-            long reading = scale_read();
-            Serial.print("HX711 reading: ");
-            Serial.println(reading);
-        } else {
-            Serial.println("Not ready");
-        }
-        /* Clock startup time is around 67ms, this gives delay of around 120ms */
-        /* TODO: test this */
-        LowPower.powerDown(SLEEP_60MS, ADC_OFF, BOD_ON);
-    }
-    delay(100);
-    scale_end();
-    digitalWrite(LED_BUILTIN, LOW);
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
 }
 
 void power_all_off()
 {
-    /* todo replace this with gsm_end */
-    sim800_power_off();
+    /* todo replace this with GSMModule */
+    SIM800PowerManager::powerOff();
 
     /* disable physical sensors */
-    scale_end();
-    dht_end();
+    ScaleModule::end();
+    DHTModule::end();
 
     /* disable rtc and twi peripheral */
-    rtc_end();
+    RTCModule::end();
 
     /* disable on chip uart and adc pheripherals */
     serial_end();
-    adc_end();
+    ADCHelper::end();
 
     /* disable spi and unnecessary timers */
     power_spi_disable();
@@ -164,24 +217,14 @@ void power_all_off()
     builtin_led_off();
 }
 
-bool user_scale_tare()
-{
-
-}
-
-bool user_scale_calibration()
-{
-
-
-
-    return true;
-}
-
 void show_result_final_block(bool success)
 {
-    serial_print(F("Operation result: "));
-    serial_print(success ? F("ok") : F("fail"));
-    serial_println(F(" | Press reset to reboot"));
+    if (serial_is_enabled())
+    {
+        Serial.print(F("Operation result: "));
+        Serial.println(success ? F("ok") : F("fail"));
+        Serial.println(F("Press reset to reboot"));
+    }
 
     power_all_off();
 
@@ -203,8 +246,9 @@ void show_result_final_block(bool success)
     do
     {
         builtin_led_on();
-        LowPower.powerDown(SLEEP_15MS, ADC_OFF, BOD_ON);
+        sleep_power_down_80ms_adc_off_bod_on();
+
         builtin_led_off();
-        LowPower.powerDown(SLEEP_120MS, ADC_OFF, BOD_ON);
+        sleep_power_down_185ms_adc_off_bod_on();
     } while (true);
 }
