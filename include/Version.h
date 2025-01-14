48df38418ed7d64a84cc726c346fdd47dbc9c0ed
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-01-14 18:32:21 +0100

    add versioning

---
+++ include/Version.h
@@ -0,0 +1,10 @@
+
+#ifndef BUILD_NUMBER
+  #define BUILD_NUMBER "6"
+#endif
+#ifndef VERSION
+  #define VERSION "v0.1.250114_6 - 2025-01-14 18:09:32.897264"
+#endif
+#ifndef VERSION_SHORT
+  #define VERSION_SHORT "v0.1.250114_6"
+#endif
