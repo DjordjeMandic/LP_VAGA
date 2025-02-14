aea357f6247745deed23ca8fd721dcba15af8f09
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-02-14 20:21:32 +0100

    fix

--- include/Version.h
+++ include/Version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "106"
+  #define BUILD_NUMBER "107"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.250213_106 - 2025-02-13 15:01:58.120949"
+  #define VERSION "v0.1.250214_107 - 2025-02-14 20:21:31.275614"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.250213_106"
+  #define VERSION_SHORT "v0.1.250214_107"
 #endif
