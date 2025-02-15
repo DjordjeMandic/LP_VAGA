2e253e64719f6d7660c102cd3e583a5c54895d48
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-02-15 22:25:31 +0100

    update ver

--- include/Version.h
+++ include/Version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "109"
+  #define BUILD_NUMBER "110"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.250215_109 - 2025-02-15 19:17:02.294952"
+  #define VERSION "v0.1.250215_110 - 2025-02-15 22:24:25.607027"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.250215_109"
+  #define VERSION_SHORT "v0.1.250215_110"
 #endif
