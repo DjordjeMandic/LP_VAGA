4a088ae6c662630c19d9a501b65e3fb265f0a3aa
Djordje <6750655+DjordjeMandic@users.noreply.github.com>
2025-02-22 22:01:47 +0100

    wip

--- include/Version.h
+++ include/Version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "152"
+  #define BUILD_NUMBER "153"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.250222_152 - 2025-02-22 21:59:42.385557"
+  #define VERSION "v0.1.250222_153 - 2025-02-22 22:01:01.104058"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.250222_152"
+  #define VERSION_SHORT "v0.1.250222_153"
 #endif
